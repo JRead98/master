01e0cb2282b1a95e6f39fad772d34b9a22ce833b
John Read <106428843+JRead98@users.noreply.github.com>
2023-06-20 16:28:10 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -80,39 +80,3 @@ class TestingSet(torch.utils.data.Dataset):
         image_testing = Image.open(self.testing_images[item]).convert('RGB')
         return self.transform(image_training), self.transform(image_testing)
 
-
-class SetThreshold(torch.utils.data.Dataset):
-
-    def __init__(self, args):
-        path = args.datapath
-        list_images = []
-        for cls in os.listdir(path):
-            for imgs in os.listdir(os.path.join(path, cls)):
-                if (args.only_jpg and imgs.endswith('.jpg')) or args.only_jpg is False:
-                    list_images.append(os.path.join(path, cls, imgs))
-        self.transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(),
-                                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                  std=[0.229, 0.224, 0.225])])
-        with open('pics', 'rb') as f:
-            training_images = pickle.load(f)
-        self.training_subset = random.sample(training_images, k=15)
-        self.testing_images = []
-        # exclusion of the training images for the selection of the second element
-        to_exclude = set(training_images)
-        for i in range(len(self.training_subset)):
-            while True:
-                candidate = random.choice(list_images)
-                if candidate not in to_exclude:
-                    self.testing_images.append(candidate)
-                    to_exclude.add(candidate)
-                    break
-        self.images = self.training_subset + self.testing_images
-
-        # defining dataset size
-    def __len__(self):
-        return len(self.images)
-
-    # selection of pairs of elements from the dataset to give to the model
-    def __getitem__(self, item):
-        image = Image.open(self.images[item]).convert('RGB')
-        return self.transform(image), self.images[item] in self.training_subset
